bfa4e57c7214036460ff32f9c286076260b64627
thiagoaxsilva <thiagoaxsilva@hotmail.com>
2021-09-06 11:05:45 -0300

    Fim do módulo

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Address, Book, Author, Country
 
 # Register your models here.
 
+
 class BookAdmin(admin.ModelAdmin):
-  prepopulated_fields = {"slug": ("title",)}
-  list_filter = ("author", "rating",)
-  list_display = ("title", "author",)
+    prepopulated_fields = {"slug": ("title",)}
+    list_filter = ("author", "rating",)
+    list_display = ("title", "author",)
+
 
 admin.site.register(Book, BookAdmin)
+admin.site.register(Author)
+admin.site.register(Address)
+admin.site.register(Country)
